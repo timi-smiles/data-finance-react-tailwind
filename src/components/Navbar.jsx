80e07a16f897488c31c75df7579829cb0d86ef66
timi-smiles <timioguns16@gmail.com>
2024-01-19 16:50:43 +0100

    fourth commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
                {/* <AiOutlineMenu size={20}></AiOutlineMenu> */}
            </div>
            <div className={nav ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500" : "ease-in-out duration-500 fixed left-[-100%]"}>
-           <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+           <h1 className="w-full text-3xl font-bold text-[#00df9a]">TIMI.tech</h1>
    
                <ul className="uppercase p-4">
                 <li className="p-4 border-b border-gray-600">Home</li>
